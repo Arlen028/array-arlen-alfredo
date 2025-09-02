13bcbaa5e329b03c137df9eee5f9d860b687178e
Arlen <arlens527@gmail.com>
2025-09-02 10:38:49 -0300

    criararray

---
+++ array_1.py
@@ -0,0 +1,12 @@
+nomes = ["joao", "maria", "jose", "pedro", "ana"]
+
+print(nomes[0])
+print(nomes[2])
+print(nomes[4])
+
+print("----------------------------------------")
+
+if "maria" in nomes:
+    print("Maria esta na lista")
+else:
+    print("Maria nao esta na lista")
